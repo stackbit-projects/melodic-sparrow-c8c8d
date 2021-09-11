6e8a87b441270176b454402e2d40ef217be8b832
Stackbit Code Editor <projects@stackbit.com>
2021-09-11 11:04:16 +0000

    blog.js: changed

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -19,6 +19,7 @@ export default class Blog extends React.Component {
 
         return (
             <Layout page={page} config={config}>
+                {data}
                 <header
                     className={classNames('section', 'section--header', {
                         'screen-reader-text': hideTitle
